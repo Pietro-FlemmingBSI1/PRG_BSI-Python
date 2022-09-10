46661fd141c5f5bc40b0d2458c7b335e9b96f878
Pietro Flemming <pietroflemming@gmail.com>
2022-09-09 21:55:28 -0300

    commit Adicionando comentários

--- embaralhaPalavra/modificador.py
+++ embaralhaPalavra/modificador.py
@@ -1,14 +1,15 @@
 from random import shuffle, choice
 
 def selecionador(lista_palavras):
+    # Lista de mensagens caso o usuário perca.
     lista_falhou = [
         "Não foi dessa vez, tente denovo, eu sei que você consegue!",
         "Essa era díficil mesmo, mas continue tentando.",
         "Essa foi por pouco, que tal tentar denovo.",
     ]
 
-    palavra = choice(lista_palavras)
-    multivacional = choice(lista_falhou)
+    palavra = choice(lista_palavras) # Escolhe a palavra que será embaralhada.
+    multivacional = choice(lista_falhou) # Escolhe a mensagem caso o usuário perca.
     return palavra, multivacional
 
 
@@ -16,10 +17,10 @@ def ordenador(palavra):
     palavra_lista = []
     palavra = palavra.lower()
     for l in palavra:
-        palavra_lista.append(l)
-    shuffle(palavra_lista)
+        palavra_lista.append(l) # Gera uma lista das letras da palavra selecionada.
+    shuffle(palavra_lista) # Coloca as letras em ordem aleatória.
     palavra_embaralhada = ""
     for l in palavra_lista:
-        palavra_embaralhada += l
+        palavra_embaralhada += l # Retorna a lista de letras em uma palavra.
     return palavra_embaralhada
 
