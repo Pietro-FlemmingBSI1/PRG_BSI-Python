70a8c7d2d03fbecf7498323bd89708ebb957c62e
Pietro Flemming <pietroflemming@gmail.com>
2022-09-20 11:31:51 -0300

    commit criando gráfico de letras

---
+++ Arquivos/arquivo.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    with open("Alice_Maravilhas.txt") as file:
+        texto = file.read().lower()
+    
+    limpa_texto = [l for l in texto if l.isalpha()]
+
+    lista_letras = {}
+
+    for l in limpa_texto:
+        lista_letras = l.get(l, 0) + 1
+    
+
+
