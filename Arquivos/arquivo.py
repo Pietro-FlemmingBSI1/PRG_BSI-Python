fa16b7e32b46978054d126c9788300049ac203ef
Pietro Flemming <pietroflemming@gmail.com>
2022-09-20 11:55:31 -0300

    commit arquivos

--- Arquivos/arquivo.py
+++ Arquivos/arquivo.py
@@ -1,4 +1,3 @@
-from matplotlib
 
 if __name__ == "__main__":
     with open("Alice_Maravilhas.txt") as file:
@@ -9,7 +8,17 @@ if __name__ == "__main__":
     lista_letras = {}
 
     for l in limpa_texto:
-        lista_letras = l.get(l, 0) + 1
+        lista_letras_ordenadas = l.get(l, 0) + 1
     
+    from collections import Counter
+    lista_letras = Counter(lista_letras)
+    import matplotlib.pyplot as plt
+
+    rotulos, valores = zip(lista_letras_ordenadas.most_common(10))
+    plt.title("Frequencia")
+    plt.bar(rotulos,valores)
+    plt.show()
+    plt.savefig("Livro.png")
+    plt.close() 
 
 
