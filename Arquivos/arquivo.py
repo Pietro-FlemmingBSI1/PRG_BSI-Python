f9e30212061a2ec53097e4d54c0b09c91ffdf512
Pietro Flemming <pietroflemming@gmail.com>
2022-09-23 11:13:20 -0300

    commit arquivo funcional

--- Arquivos/arquivo.py
+++ Arquivos/arquivo.py
@@ -8,14 +8,14 @@ if __name__ == "__main__":
     lista_letras = {}
 
     for l in limpa_texto:
-        lista_letras_ordenadas = l.get(l, 0) + 1
+        lista_letras[l] = lista_letras.get(l, 0) + 1
     
     from collections import Counter
     lista_letras = Counter(lista_letras)
     import matplotlib.pyplot as plt
 
-    rotulos, valores = zip(lista_letras_ordenadas.most_common(10))
-    plt.title("Frequencia")
+    rotulos, valores = zip(*lista_letras.most_common(50))
+    plt.title("Frequencia de letras")
     plt.bar(rotulos,valores)
     plt.show()
     plt.savefig("Livro.png")
