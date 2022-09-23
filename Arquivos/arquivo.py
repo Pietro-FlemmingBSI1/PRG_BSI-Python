626ee5892bec3609606376c14fbd973efbd9c0fc
Pietro Flemming <pietroflemming@gmail.com>
2022-09-23 11:26:36 -0300

    commit adicionando funções

--- Arquivos/arquivo.py
+++ Arquivos/arquivo.py
@@ -1,20 +1,26 @@
-
-if __name__ == "__main__":
+def leitor_texto():
     with open("Alice_Maravilhas.txt") as file:
         texto = file.read().lower()
-    
-    limpa_texto = [l for l in texto if l.isalpha()]
+    return texto
 
+def quantidade_letras(texto):
     lista_letras = {}
 
-    for l in limpa_texto:
+    for l in texto:
         lista_letras[l] = lista_letras.get(l, 0) + 1
-    
     from collections import Counter
     lista_letras = Counter(lista_letras)
+
+    return lista_letras
+
+
+if __name__ == "__main__":    
+
     import matplotlib.pyplot as plt
 
-    rotulos, valores = zip(*lista_letras.most_common(50))
+    texto = [l for l in leitor_texto() if l.isalpha()]
+
+    rotulos, valores = zip(*quantidade_letras(texto).most_common(50))
     plt.title("Frequencia de letras")
     plt.bar(rotulos,valores)
     plt.show()
